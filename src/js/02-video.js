771d53bf9df0b9ffe1afd9b40689cb8ccd6bbe79
Utilizator <Stanislavbox24@gmail.com>
2023-04-07 20:38:22 +0300

    task 2, task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const player = new Player("vimeo-player");
+const currentTime = localStorage.getItem("videoplayer-current-time");
+
+const onPlay = function(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
 
+const formEl = document.querySelector('.feedback-form');
+const emaiEl = document.querySelector('input[name = "email"]');
+const messageEl = document.querySelector('textarea[name = "message"]');
+
+const savelocalHost = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (savelocalHost){
+    emaiEl.value = savelocalHost.email;
+    messageEl.value = savelocalHost.message;
+}
+
+formEl.addEventListener('input', throttle(onData, 500));
+
+formEl.addEventListener('submit', onClear)
+
+function onData (){
+    const dataForm ={
+        email: emaiEl.value,
+        message: messageEl.value
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(dataForm));
+}
+
+function onClear(evt){
+    evt.preventDefault();
+    console.log(`email: ${emaiEl.value} messege: ${messageEl.value}`)
+
+    localStorage.removeItem('feedback-form-state');
+
+    emaiEl.value ='';
+    messageEl.value ='';
+}
