948ff774642658d2569bb7b291891dacf594f41b
Utilizator <Stanislavbox24@gmail.com>
2023-04-07 11:21:17 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+
+// ? CREAT MARKUP
+const markupItemEl = galleryItems.map(({preview, original, description}) => 
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                  <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+      </li>`
+).join('');
+
+listEl.insertAdjacentHTML('beforeend', markupItemEl)// ADD MARKUP IN HTML
+
+
+// ? INITIAL LIGHTBOX AND ADD OPTIONS
+var lightbox = new SimpleLightbox('.gallery a', {
+      captions: true,
+      captionDelay: 250,
+      captionsData: 'alt'
+ });
